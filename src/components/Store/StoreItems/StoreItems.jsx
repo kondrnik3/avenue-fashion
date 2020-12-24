78ddda183e4a41dabd6f7171c4283c847ad5ca4d
kondrnik3 <66358369+kondrnik3@users.noreply.github.com>
2020-12-24 19:27:42 +0400

    First commit

---
+++ src/components/Store/StoreItems/StoreItems.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import photoStore01 from "../../assets/img/store_photo01.jpg";
+import photoStore03 from "../../assets/img/store_photo03.jpg";
+import StoreItem from "./StoreItem";
+
+let StoreItems = () => {
+    return (
+        <div className="store_items">
+               <StoreItem/>
+        </div>
+    );
+};
+export default StoreItems
